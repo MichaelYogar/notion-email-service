98c746d25e7a43c0f836976147b217af4f684849
Michael Yogar <michaelyogar878@gmail.com>
2021-06-07 21:16:15 -0400

    refactor: replaced axios with notion sdk and updated functions

--- src/notion/notion-controller.ts
+++ src/notion/notion-controller.ts
@@ -1,54 +1,45 @@
 import { Router, Request, Response, NextFunction } from 'express';
-import axios, { AxiosInstance } from 'axios';
+import { Client } from '@notionhq/client';
 import Logger from '../logger';
 import BaseException from '../exceptions/base-exception';
 import Controller from '../interfaces/controller-interface';
-import Constants from '../config/constants';
 
 class NotionController implements Controller {
-  #api: AxiosInstance;
+  #api: Client;
 
   public path = '/notion';
 
   public router: Router = Router();
 
+  #key: string = process.env.NOTION_API_KEY as string;
+
   constructor() {
     this.initializeRoutes();
-    this.#api = axios.create({
-      baseURL: Constants.NOTION_PREFIX,
-      headers: {
-        Authorization: `Bearer ${process.env.NOTION_API_KEY}`,
-        'Notion-Version': Constants.NOTION_VERSION,
-      },
+    // Initializing a client
+    this.#api = new Client({
+      auth: this.#key,
     });
   }
 
   private initializeRoutes() {
-    this.router.get(`${this.path}/getDatabaseList`, this.getDatabaseList);
+    // get requests
     this.router.get(`${this.path}/getDatabase`, this.getDatabase);
     this.router.get(`${this.path}/getPage`, this.getPage);
     this.router.get(`${this.path}/getBlockChildren`, this.getBlockChildren);
     this.router.get(`${this.path}/getUser`, this.getUser);
+    this.router.get(`${this.path}/getUserList`, this.getUserList);
   }
-
-  private getDatabaseList = async (req: Request, res: Response, next: NextFunction) => {
-    Logger.info('getDatabaseList called');
-
-    try {
-      const resp = await this.#api.get(`/databases`);
-      res.send(resp.data);
-    } catch (err: any) {
-      Logger.error(err);
-      next(new BaseException(400, err.message));
-    }
-  };
 
   private getDatabase = async (req: Request, res: Response, next: NextFunction) => {
     Logger.info('getDatabase called');
 
+    const databaseId = '';
+
     try {
-      const resp = await this.#api.get(`/databases/${process.env.DATABASE_ID}`);
-      res.send(resp.data);
+      const resp = await this.#api.databases.retrieve({
+        database_id: databaseId,
+      });
+      res.send(resp);
     } catch (err: any) {
       Logger.error(err);
       next(new BaseException(400, err.message));
@@ -58,9 +49,13 @@ class NotionController implements Controller {
   private getBlockChildren = async (req: Request, res: Response, next: NextFunction) => {
     Logger.info('getBlockChildren called');
 
+    const blockId = '';
     try {
-      const resp = await this.#api.get(`/blocks/${process.env.BLOCK_ID}/children`);
-      res.send(resp.data);
+      const resp = await this.#api.blocks.children.list({
+        block_id: blockId,
+        page_size: 50,
+      });
+      res.send(resp);
     } catch (err: any) {
       Logger.error(err);
       next(new BaseException(400, err.message));
@@ -70,9 +65,22 @@ class NotionController implements Controller {
   private getUser = async (req: Request, res: Response, next: NextFunction) => {
     Logger.info('getUser called');
 
+    const userId = '';
     try {
-      const resp = await this.#api.get(`/users/${process.env.USER_ID}`);
-      res.send(resp.data);
+      const resp = await this.#api.users.retrieve({ user_id: userId });
+      res.send(resp);
+    } catch (err: any) {
+      Logger.error(err);
+      next(new BaseException(400, err.message));
+    }
+  };
+
+  private getUserList = async (req: Request, res: Response, next: NextFunction) => {
+    Logger.info('getUserList called');
+
+    try {
+      const resp = await this.#api.users.list();
+      res.send(resp);
     } catch (err: any) {
       Logger.error(err);
       next(new BaseException(400, err.message));
@@ -82,8 +90,9 @@ class NotionController implements Controller {
   private getPage = async (req: Request, res: Response, next: NextFunction) => {
     Logger.info('getPage called');
     try {
-      const resp = await this.#api.get(`/pages/${process.env.PAGE_ID}`);
-      res.send(resp.data);
+      const pageId = '';
+      const resp = await this.#api.pages.retrieve({ page_id: pageId });
+      res.send(resp);
     } catch (err: any) {
       Logger.error(err);
       next(new BaseException(400, err.message));
